14b3b883096eaff0e6d94d613941ddbfe90fcffa
Eiji YAMANE <eijiyamane@gmail.com>
2012-06-06 10:42:12 +0900

    CallbackEventListenerSpec.scalaを追加。但し、責務分割が怪しいのでテストはpending
    Signed-off-by: Eiji YAMANE <eijiyamane@gmail.com>

---
+++ src/test/scala/CallbackEventListenerSpec.scala
@@ -0,0 +1,16 @@
+import org.scalatest.FunSpec
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: e-yamane
+ * Date: 12/06/06
+ * Time: 10:40
+ * To change this template use File | Settings | File Templates.
+ */
+
+class CallbackEventListenerSpec extends FunSpec {
+    describe("CallbackEventListenerの仕様") {
+        it("責務分割が怪しいのでpending") (pending)
+    }
+
+}
