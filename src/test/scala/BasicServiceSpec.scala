a02b1f07a47fd46953727931be381113ff34b0d0
Eiji YAMANE <eijiyamane@gmail.com>
2012-06-07 17:18:20 +0900

    CustomValidate（同一Entity内）のテスト追加完了。バグが見つかった... orz
    Signed-off-by: Eiji YAMANE <eijiyamane@gmail.com>

--- src/test/scala/BasicServiceSpec.scala
+++ src/test/scala/BasicServiceSpec.scala
@@ -1,11 +1,11 @@
 import java.io.Serializable
 import java.lang.Long
-import java.util.{Arrays, Map, ArrayList, List}
+import java.util._
 import jp.rough_diamond.commons.di.DIContainerTestingHelper
 import jp.rough_diamond.commons.di.DIContainerTestingHelper.DIHook
 import jp.rough_diamond.commons.extractor.{FreeFormat, ExtractValue, Extractor}
-import jp.rough_diamond.commons.resource.{ResourceManager, LocaleControllerByThreadLocal, LocaleController}
-import jp.rough_diamond.commons.service.annotation.{NestedComponent, MaxCharLength, MaxLength, NotNull}
+import jp.rough_diamond.commons.resource._
+import jp.rough_diamond.commons.service.annotation._
 import jp.rough_diamond.commons.service.BasicService.RecordLock
 import jp.rough_diamond.commons.service.{WhenVerifier, FindResult, BasicService}
 import org.scalatest.matchers.ShouldMatchers
@@ -335,6 +335,68 @@ class BasicServiceSpec extends FunSpec with ShouldMatchers with BeforeAndAfter {
                     msg.get("Y.name").get(0).getKey should be ("errors.maxlength")
                 }
             }
+            describe("CustomeValidate（同一Entity）の仕様") {
+                it("INSERTの検証はINSERTのもののみpriorityの降順である事") {
+                    val bean = new ValidateTestBean
+                    val service = new BasicServiceExt
+
+                    bean.reqInfo = "x"
+                    val msg = service.validate(bean, WhenVerifier.INSERT)
+
+                    bean.stack.size() should be (4)
+                    bean.stack.toArray should be (Array("2", "3", "4", "1"))
+                }
+                it("UPDATEの時はUPDATEのもののみでpriorityの降順である事") {
+                    val bean = new ValidateTestBean
+                    val service = new BasicServiceExt
+
+                    bean.reqInfo = "x"
+                    val msg = service.validate(bean, WhenVerifier.UPDATE)
+
+                    bean.stack.size() should be (4)
+                    bean.stack.toArray should be (Array("2", "3", "5", "1"))
+                }
+                it("forceExec=falseもしくは省略した場合、エラーが発生していればvalidateを中止する事") {
+                    val bean = new ValidateTestBean
+                    val service = new BasicServiceExt
+
+                    bean.reqInfo = "x"
+                    bean.name = "foo"
+                    val msg = service.validate(bean, WhenVerifier.UPDATE)
+
+                    bean.stack.size() should be (2)
+                    bean.stack.toArray should be (Array("2", "3"))
+                }
+                it("forceExec=trueの場合、エラーが発生してもvalidateは継続する事") {
+                    val bean = new ValidateTestBean
+                    val service = new BasicServiceExt
+
+                    bean.reqInfo = "x"
+                    bean.code = 27
+                    bean.name = "foo"
+                    val msg = service.validate(bean, WhenVerifier.UPDATE)
+
+                    bean.stack.size() should be (2)
+                    bean.stack.toArray should be (Array("2", "3"))
+
+                    msg.get("Z.code").size() should be (1)
+                    msg.get("Z.name").size() should be (1)
+                }
+                it("NestedComponentのValidateはOuterのvalidate後に呼び出される事") {
+                    val bean = new ValidateTestBeanOuter
+                    val service = new BasicServiceExt
+
+                    bean.bean = new ValidateTestBean
+                    val msg = service.validate(bean, WhenVerifier.UPDATE)
+
+                    msg.hasError should be (true)
+
+                    println(bean.bean.stack)
+
+                    bean.bean.stack.size() should be (3)
+                    bean.bean.stack.toArray should be (Array("outer", "2", "3"))
+                }
+            }
         }
     }
 
@@ -364,6 +426,46 @@ class BasicServiceSpec extends FunSpec with ShouldMatchers with BeforeAndAfter {
         def getReqInfo : String = {
             reqInfo
         }
+
+        val stack = new Stack[String]
+
+        @Verifier(priority = 1, isForceExec = false)
+        def validate1 : Messages = {
+            stack.push("1")
+            new Messages
+        }
+
+        @Verifier(priority = 10, isForceExec = true)
+        def validate2 : Messages = {
+            stack.push("2")
+            val ret = new Messages()
+            if(code == 27) {
+                ret.add("Z.code", new Message("Z.code"))
+            }
+            return ret
+        }
+
+        @Verifier(priority = 5, isForceExec = true)
+        def validate3 : Messages = {
+            stack.push("3")
+            val ret = new Messages()
+            if(name == "foo") {
+                ret.add("Z.name", new Message("Z.name"))
+            }
+            return ret
+        }
+
+        @Verifier(priority = 4, when=Array(WhenVerifier.INSERT))
+        def validate4 : Messages = {
+            stack.push("4")
+            new Messages
+        }
+
+        @Verifier(priority = 4, when=Array(WhenVerifier.UPDATE))
+        def validate5 : Messages = {
+            stack.push("5")
+            new Messages
+        }
     }
 
     class ValidateTestBeanOuter {
@@ -379,6 +481,14 @@ class BasicServiceSpec extends FunSpec with ShouldMatchers with BeforeAndAfter {
         @MaxLength(length = 3, property = "Y.name")
         def getName : String = {
             name
+        }
+
+        @Verifier(isForceExec = true)
+        def validateOuter : Messages = {
+            if(bean != null) {
+                bean.stack.push("outer")
+            }
+            return new Messages
         }
     }
 
